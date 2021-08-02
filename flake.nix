dcc519fc21e1361d1aeca4b1de64cd3ac361561a
A Raghuram <raghurammailbox@gmail.com>
2021-08-03 00:05:33 +0530

    remove flake.lock10

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
  description = "searx : flake";
-inputs = { nixpkgs.url = github:NixOS/nixpkgs/nixos-20.03;   };
+inputs = { nixpkgs.url = github:NixOS/nixpkgs/nixos-21.05;   };
 
 outputs = inputs: { defaultPackage.x86_64-linux = import ./default.nix {nixpkgs =inputs.nixpkgs; }; };
   
