67fb63a647076affc8a04ed123747c7feb1b587d
A Raghuram <raghurammailbox@gmail.com>
2021-08-02 23:58:54 +0530

    remove flake.lock9

--- flake.nix
+++ flake.nix
@@ -1,30 +1,8 @@
 {
  description = "searx : flake";
-inputs = {
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-20.03;
-  inputs.buildInputs = [
-      nixpkgs.python39
-      nixpkgs.git
-      nixpkgs.openssl
-      nixpkgs.python39Packages.pip
-      nixpkgs.uwsgi
-      nixpkgs.python39Packages.virtualenv
-      nixpkgs.poetry
-     ];
- };
-  outputs = { self, nixpkgs }: 
-
-
-
-
-
-nixpkgs.poetry2nix.mkPoetryApplication {
-  projectDir = ./.;
-      src = nixpkgs.fetchgit {
-        url = "https://github.com/searx/searx.git";
-        rev = "ae122ea943f77600fd97556503c483dcd92e1e63";
-        sha256 = "sIJ+QXwUdsRIpg6ffUS3ItQvrFy0kmtI8whaiR7qEz4=";
-      };
+inputs = { nixpkgs.url = github:NixOS/nixpkgs/nixos-20.03;   };
+  outputs = inputs: {
+     defaultPackage.x86_64-linux = import ./default.nix {nixpkgs =inputs.nixpkgs; }
 
 
 };
